68fb3b45e4fcb7cfdfb23ba84104bf5e54bbb291
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-02-22 16:57:23 +0900

    Queue_rotation, doky(description)

--- Queue/baekjoon12789_doky.cpp
+++ Queue/baekjoon12789_doky.cpp
@@ -1,36 +1,37 @@
 #include<iostream>
-#include<string>
 #include<stack>
 #include<queue>
 
 using namespace std;
 
 int main(){
-    int number;
+    int number;                                 // 학생들의 수
+    int list;                                   // 번호표
+    queue<int> q;                               // 현재 줄서 있는곳
+    stack<int> st;                              // 한명씩만 설수있는공간
+
     cin>>number;
-    int list;
-    queue<int> q;
-    stack<int> st;
     for(int i=0;i<number;i++){
         cin>>list;
         q.push(list);
     }
-    int count=1;
+
+    int count=1;                                // 간식받는 순서
     while(1){
-        if(!q.empty()&&q.front()==count){
+        if(!q.empty()&&q.front()==count){       // q의 앞과 순서가 일치할경우 pop,count++
             q.pop();
             count++;
-        }else if(!st.empty()&&st.top()==count){
+        }else if(!st.empty()&&st.top()==count){ // 한명씩 설수있는 공간의 앞과 순서와 일치할경우 pop,count++
             st.pop();
             count++;
-        }else if(q.empty()){
+        }else if(q.empty()){                    // 앞선 두 경우를 확인하고 q에 더이상 빠질 학생수가없다면 sad
             cout<<"Sad";
             break;
-        }else{
+        }else{                                  // q에 아직 원소가 남아있다면 stack으로 push
             st.push(q.front());
             q.pop();
         }
-        if(count>number){
+        if(count>number){                       // 순서가 학생수보다 더큰경우 Nice
             cout<<"Nice";
             break;
         }
