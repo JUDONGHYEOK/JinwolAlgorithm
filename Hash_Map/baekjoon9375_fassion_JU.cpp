d016f3bbe3cf3d78472a38ee8bfd65f563a2026d
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-02-25 14:36:25 +0900

    Hash

--- Hash_Map/baekjoon9375_fassion_JU.cpp
+++ Hash_Map/baekjoon9375_fassion_JU.cpp
@@ -5,31 +5,6 @@
 
 using namespace std;
 
-int solution(vector<vector<string>> clothes) {
-    int answer = 1;
-    unordered_map<string,int> allClothes;
-    pair<string,int> cloth;
-    string cl,ca;
-    
-    for(int i=0,j=clothes.size();i<j;i++){
-        if(allClothes.count(clothes[i][1])){
-            auto it=allClothes.find(clothes[i][1]);
-            int number=it->second+1;
-            allClothes.erase(clothes[i][1]);
-            cloth={clothes[i][1],number};
-            allClothes.insert(cloth);
-        }else{
-            cloth={clothes[i][1],1};
-            allClothes.insert(cloth);
-        }
-    }
-    for(auto it=allClothes.begin();it!=allClothes.end();it++){
-        answer*=(it->second+1);
-    }
-    answer-=1;
-    return answer;
-}
-
 int main(){
     int number,size;
     string clothes,category;
