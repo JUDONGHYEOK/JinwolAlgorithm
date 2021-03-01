be3019cbf09eef6dafc7956aae4e74f4047d9a7f
WanUk <parkwanwook@naver.com>
2021-03-01 16:43:36 +0900

    hash

--- Hash_Map/17219_password_hash_wook.py
+++ Hash_Map/17219_password_hash_wook.py
@@ -1,9 +1,10 @@
 n, m = map(int,input().split())
-password = {}
+password = {} #딕셔너리 생성
 for i in range(n):
     a, b = map(str,input().split())
-    password[a] = b
+    password[a] = b #딕셔너리에서 a를 key값, b를 value 값으로 저장
+    
 
 for i in range(m):
-    c = str(input())
-    print(password[c])
+    c = str(input()) #사이트 입력
+    print(password[c])#사이트에 맞는 value값 출력
